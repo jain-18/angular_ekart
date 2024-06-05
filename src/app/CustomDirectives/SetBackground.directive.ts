fec087eef5ab5079c6ace8636a4a8855f1aa85d5
jain-18 <shubhamjain12360@gmail.com>
2024-06-05 13:40:35 +0530

    day 7 :  Property binding

--- src/app/CustomDirectives/SetBackground.directive.ts
+++ src/app/CustomDirectives/SetBackground.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Renderer2 } from "@angular/core";
+import { Directive, ElementRef, Input, Renderer2 } from "@angular/core";
 
 @Directive({
     selector:'[setBackground]'
@@ -6,6 +6,10 @@ import { Directive, ElementRef, Renderer2 } from "@angular/core";
 export class SetBackground{
     // private element : ElementRef;
     // private renderer : Renderer2;
+    // @Input() backColor : String = '#36454F';
+    // @Input() textColor : string = 'white';
+
+    @Input('setBackground') changeTextAndBackClor : {backColor : String , textColor : string};
 
     constructor(private element : ElementRef,private renderer : Renderer2){
             // this.element = element;
@@ -16,8 +20,9 @@ export class SetBackground{
         // this.element.nativeElement.style.backgroundColor = '#36454F'
         // this.element.nativeElement.style.color = 'white'
 
-        this.renderer.setStyle(this.element.nativeElement,'background','#36454F')
-        this.renderer.setStyle(this.element.nativeElement,'color','white')
-        this.renderer.setAttribute(this.element.nativeElement,'title','This is example title')
+        this.renderer.setStyle(this.element.nativeElement,'background',this.changeTextAndBackClor.backColor)
+        this.renderer.setStyle(this.element.nativeElement,'color',this.changeTextAndBackClor.textColor)
+
+        // this.renderer.setAttribute(this.element.nativeElement,'title','This is example title')
     }
 }
